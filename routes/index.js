2bfc307e758ecf30655b2b8b9bfc5344444d62fe
abhiram304 <abhiram.304@gmail.com>
2016-11-06 17:55:06 -0800

    Display lattitude commit1

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,26 @@
 /*
  * GET home page.
  */
+var mysql = require('./mysql');
 
 exports.index = function(req, res){
-	var lat=req.param("Lat");
-	var lng=req.param("Lng");
+	var lat=req.body.Lat;
+	var lng=req.body.Lng;
+	/*var queryStr = "INSERT INTO User VALUES('" + detailsP.UserId + "', '"
+	+ detailsP.UserName + "', '" + detailsP.Password + "', '"
+	+ shortid.generate() + "', '" + detailsP.PrimaryTenant + "', '"
+	+ detailsP.Age + "', '" + detailsP.Gender + "', '"
+	+ Number(detailsP.Contact) + "', '" + Number(detailsP.Emergencyc)
+	+ "')";
+	mysql.fetchData(function(err, results) {
+		if (err) {
+			console.log("Error in db connection: "+err);
+			throw err;
+		} else {
+			res.render('home', { title: lat });
+			res.send({dataSaved: true});
+		}
+	}, queryStr);*/
 	console.log("The api is hit: "+lat);
-	res.render('index', { title: lat });
+	res.render('home', { title: lat });
 };
