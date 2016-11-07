88674ebecc3dd048c0a8cd7be764cdb32a5f17a7
abhiram304 <abhiram.304@gmail.com>
2016-11-06 16:23:34 -0800

    First commit

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@
  */
 
 exports.index = function(req, res){
-	var lat=req.body.Lat;
-	var lng=req.body.Lng;
+	var lat=req.param("Lat");
+	var lng=req.param("Lng");
 	console.log("The api is hit: "+lat);
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: lat });
 };
